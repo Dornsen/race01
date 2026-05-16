e1d3c72d0e89dc6a72745d4ba3b3e573b28b3c0d
Zakhar <zmatveev.20112006@gmail.com>
2026-05-17 00:33:19 +0300

    1

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ const { Server } = require('socket.io');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader(
+        'Content-Security-Policy',
+        "default-src 'self'; img-src 'self' data: blob: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src 'self' ws: wss: http: https:; font-src 'self' data:;"
+    );
+    next();
+});
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client')));
 
